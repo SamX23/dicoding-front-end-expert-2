ff2b9667b6977933a6ff1b59b91ef689367543ac
SamX23 <skalexsong@gmail.com>
2021-01-06 22:10:17 +0700

    generate cards from json, and set up cards template

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,11 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import Data from "../DATA.json";
 
-console.log('Hello Coders! :)');
+const data = Data.restaurants;
+
+data.forEach((id) => {
+  document.getElementById(
+    "card"
+  ).innerHTML += `<div class='card__item'><div class='card__image'><img src='${id.pictureId}' alt='${id.name}' /><p>${id.city}</p></div><div class='card__content'><h4>${id.name}</h4><p>${id.description}</p></div></div></div>`;
+});
