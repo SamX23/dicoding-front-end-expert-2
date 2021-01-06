a411a419281946fe39287b2b1fa42bc4dcb7b86e
SamX23 <skalexsong@gmail.com>
2021-01-06 23:13:25 +0700

    add basic responsive css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
+import "../styles/responsive.css";
 import Data from "../DATA.json";
 
 const data = Data.restaurants;
