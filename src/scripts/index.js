0f8e3b4f81c60adbe24aeee39d0e0435a0fc9481
SamX23 <skalexsong@gmail.com>
2021-01-07 21:20:26 +0700

    navbar fixed scroll

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,10 +3,14 @@ import "../styles/main.css";
 import "../styles/responsive.css";
 import Data from "../DATA.json";
 
-const data = Data.restaurants;
+function GenerateCards() {
+  const data = Data.restaurants;
 
-data.forEach((id, idx) => {
-  document.getElementById(
-    "card"
-  ).innerHTML += `<div id=${idx} class='card__item'><div class='card__image'><img src=${id.pictureId} alt=${id.name} /><span class='card__imageLeft'>${id.city}</span><span class='card__imageRight'>${id.rating}</span></div><div class='card__content'><h4>${id.name}</h4><p>${id.description}</p></div></div></div>`;
-});
+  data.forEach((id, idx) => {
+    document.getElementById(
+      "card"
+    ).innerHTML += `<div id=${idx} class='card__item'><div class='card__image'><img src=${id.pictureId} alt=${id.name} /><span class='card__imageLeft'>${id.city}</span><span class='card__imageRight'>${id.rating}</span></div><div class='card__content'><h4>${id.name}</h4><p>${id.description}</p></div></div></div>`;
+  });
+}
+
+GenerateCards();
