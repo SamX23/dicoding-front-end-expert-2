cb22127f441e4052a6d5dc7b68f95d5ec8f0ca01
SamX23 <skalexsong@gmail.com>
2021-01-07 22:16:37 +0700

    functioning drawer and fix media query on max width 450px

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,4 +13,21 @@ function GenerateCards() {
   });
 }
 
+function ShowDrawer() {
+  const hamburgerButtonElement = document.querySelector("#hamburger");
+  const drawerElement = document.querySelector("#drawer");
+  const mainElement = document.querySelector("main");
+
+  hamburgerButtonElement.addEventListener("click", (event) => {
+    drawerElement.classList.toggle("open");
+    event.stopPropagation();
+  });
+
+  mainElement.addEventListener("click", (event) => {
+    drawerElement.classList.remove("open");
+    event.stopPropagation();
+  });
+}
+
 GenerateCards();
+ShowDrawer();
