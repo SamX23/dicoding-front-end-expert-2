48aa8b881af932271057b59685abfec3dd36c5eb
samx23 <skalexsong@gmail.com>
2021-01-20 13:57:40 +0700

    add page views

---
+++ src/scripts/views/detail.js
@@ -0,0 +1,9 @@
+const Details = {
+  async render() {
+    return `<h2>Details page</h2>`;
+  },
+
+  async afterRender() {},
+};
+
+export default Details;
---
+++ src/scripts/views/favorit.js
@@ -0,0 +1,9 @@
+const Favourite = {
+  async render() {
+    return `<h2>Render page</h2>`;
+  },
+
+  async afterRender() {},
+};
+
+export default Favourite;
