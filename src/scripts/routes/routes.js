2a79022c4868842788ed48e0cd8656fc14cde043
samx23 <skalexsong@gmail.com>
2021-01-20 18:04:58 +0700

    create url parser and router

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,11 @@
+import Detail from "../views/pages/detail";
+import Favourite from "../views/pages/favourite";
+import Home from "../views/pages/home";
+
+const routes = {
+  "/": Home,
+  "/favourite": Favourite,
+  "/detail/:id": Detail,
+};
+
+export default routes;
