3e0a0c6480baa17c7ed9dd9d41a34c87e5064b4d
samx23 <skalexsong@gmail.com>
2021-01-23 21:02:59 +0700

    add app index and render pages

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,21 @@
-import "regenerator-runtime"; /* for async await transpile */
+import "regenerator-runtime";
+import App from "./views/app";
+import RegisterSW from "./utils/sw-register";
 import "../styles/main.css";
 import "../styles/responsive.css";
-import Data from "../DATA.json";
 
-function GenerateCards() {
-  const data = Data.restaurants;
+const app = new App({
+  button: document.querySelector("#hamburger"),
+  drawer: document.querySelector("#drawer"),
+  content: document.querySelector("#main"),
+});
 
-  data.forEach((id, idx) => {
-    document.getElementById(
-      "card"
-    ).innerHTML += `<div id=${idx} class='card__item'><div class='card__image'><img src=${id.pictureId} alt=${id.name} /><span class='card__imageLeft'>${id.city}</span><span class='card__imageRight'>${id.rating}</span></div><div class='card__content'><h3>${id.name}</h3><p>${id.description}</p></div></div></div>`;
-  });
-}
+window.addEventListener("hashchange", () => {
+  app.renderPage();
+});
 
-function ShowDrawer() {
-  const hamburgerButtonElement = document.querySelector("#hamburger");
-  const drawerElement = document.querySelector("#drawer");
-  const mainElement = document.querySelector("main");
-
-  hamburgerButtonElement.addEventListener("click", (event) => {
-    drawerElement.classList.toggle("open");
-    event.stopPropagation();
-  });
-
-  mainElement.addEventListener("click", (event) => {
-    drawerElement.classList.remove("open");
-    event.stopPropagation();
-  });
-}
-
-GenerateCards();
-ShowDrawer();
+window.addEventListener("load", () => {
+  app.renderPage();
+  RegisterSW();
+  // WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
+});
