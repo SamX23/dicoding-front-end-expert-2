e74ed465b00de5dc3b0a6f00323710ab41a993da
samx23 <skalexsong@gmail.com>
2021-01-23 21:02:30 +0700

    add app index and render pages

---
+++ src/scripts/views/app.js
@@ -0,0 +1,33 @@
+import Drawer from "../utils/show-drawer";
+import GenerateCards from "../utils/cards-generator";
+import UrlParser from "../routes/url-parser";
+import routes from "../routes/routes";
+import Data from "../../DATA.json";
+
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+    this._initialAppShell();
+  }
+
+  _initialAppShell() {
+    Drawer({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+
+    GenerateCards(Data);
+  }
+
+  async renderPage() {
+    const url = UrlParser.parseActiveUrlWithCombiner();
+    const page = routes[url];
+    this._content.innerHTML = await page.render();
+    await page.afterRender();
+  }
+}
+
+export default App;
