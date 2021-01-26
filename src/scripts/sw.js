e44ac67aef090ac95e471326d14e77caac057149
samx23 <skalexsong@gmail.com>
2021-01-26 10:50:15 +0700

    finish service worker command

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,15 +1,19 @@
 import "regenerator-runtime";
+import CacheHelper from "./utils/cache-helper";
+
+const { assets } = global.serviceWorkerOption;
 
 self.addEventListener("install", (event) => {
   console.log("Installing Service Worker ...");
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, "./"]));
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Activating Service Worker ...");
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
+  console.log("Fetching..");
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
