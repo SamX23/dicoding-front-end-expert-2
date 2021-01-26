3c191ebd5cc9291e5ff22024a44312b6c1ce8484
samx23 <skalexsong@gmail.com>
2021-01-27 06:11:40 +0700

    fix card component html

--- src/scripts/views/template/cards-generator.js
+++ src/scripts/views/template/cards-generator.js
@@ -4,9 +4,18 @@ const GenerateCards = (Data) => {
   if (Data && Data.length > 0) {
     Data.forEach((id, idx) => {
       const MediumImg = `${ENDPOINT.IMG.M}${id.pictureId}`;
-      document.getElementById(
-        "card"
-      ).innerHTML += `<div id=${idx} class='card__item'><div class='card__image'><img src=${MediumImg} alt=${id.name} /><span class='card__imageLeft'>${id.city}</span><span class='card__imageRight'>${id.rating}</span></div><div class='card__content'><h3>${id.name}</h3><p>${id.description}</p></div></div></div>`;
+      document.getElementById("card").innerHTML += `
+      <div id=${idx} class='card__item'>
+        <div class='card__image'>
+          <img src=${MediumImg} alt=${id.name} />
+          <span class='card__imageLeft'>${id.city}</span>
+          <span class='card__imageRight'>${id.rating}</span>
+        </div>
+        <div class='card__content'>
+          <h3>${id.name}</h3>
+          <p>${id.description}</p>
+        </div>
+      </div>`;
     });
   }
 };
