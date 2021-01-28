a88f8133c5e5ce3615e3f910f355b0f4b4100d33
samx23 <skalexsong@gmail.com>
2021-01-28 23:58:44 +0700

    fix on item creation

--- src/scripts/views/pages/favourite.js
+++ src/scripts/views/pages/favourite.js
@@ -1,19 +1,21 @@
-import GenerateCards from "../template/cards-generator";
+import GenerateCards from "../template/creator";
 import FavourieDishIdb from "../../data/object-db";
 
 const Favourite = {
   async render() {
     return `
     <div class="content">
-      <h2 class="content__heading">Your Liked Movie</h2>
-      <div id="movies" class="movies"></div>
+      <h2 class="content__heading">Your Liked Dish</h2>
+      <div id="favourite" class="favourite"></div>
     </div>`;
   },
 
   async afterRender() {
     const favourite = await FavourieDishIdb.getAllDish();
-    const moviesContainer = document.querySelector("#movies");
-    moviesContainer.innerHTML = GenerateCards(favourite);
+    const favouriteContainer = document.querySelector("#favourite");
+    favourite.forEach((dish) => {
+      favouriteContainer.innerHTML += GenerateCards(dish);
+    });
   },
 };
 
