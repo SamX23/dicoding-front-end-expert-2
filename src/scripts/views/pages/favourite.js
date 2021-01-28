1d0968ee90f14b367f0eb21c4d8564d27a89145f
samx23 <skalexsong@gmail.com>
2021-01-28 23:10:43 +0700

    add favourite pages content

--- src/scripts/views/pages/favourite.js
+++ src/scripts/views/pages/favourite.js
@@ -1,10 +1,19 @@
+import GenerateCards from "../template/cards-generator";
+import FavourieDishIdb from "../../data/object-db";
+
 const Favourite = {
   async render() {
-    return `<h2>Render page</h2>`;
+    return `
+    <div class="content">
+      <h2 class="content__heading">Your Liked Movie</h2>
+      <div id="movies" class="movies"></div>
+    </div>`;
   },
 
   async afterRender() {
-    return ``;
+    const favourite = await FavourieDishIdb.getAllDish();
+    const moviesContainer = document.querySelector("#movies");
+    moviesContainer.innerHTML = GenerateCards(favourite);
   },
 };
 
