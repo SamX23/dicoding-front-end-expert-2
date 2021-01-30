0fec5415f9bccad6d74d41645d50da135e912901
samx23 <skalexsong@gmail.com>
2021-01-30 12:43:54 +0700

    add arguments for details URL id

--- src/scripts/globals/api-endpoints.js
+++ src/scripts/globals/api-endpoints.js
@@ -2,7 +2,7 @@ import ENV from "./config";
 
 const ENDPOINT = {
   LIST: `${ENV.BASE_URL}/list`,
-  DETAIL: `${ENV.BASE_URL}/detail/:id`,
+  DETAIL: (id) => `${ENV.BASE_URL}/detail/${id}`,
   IMG: {
     S: `${ENV.BASE_URL}/images/small/`,
     M: `${ENV.BASE_URL}/images/medium/`,
