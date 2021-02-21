50cc8c2b8bedbe52579267c01c7db17548890493
samx23 <skalexsong@gmail.com>
2021-02-21 15:32:12 +0700

    add responsive image plugin

--- webpack.common.js
+++ webpack.common.js
@@ -35,8 +35,11 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/public/"),
-          to: path.resolve(__dirname, "dist/"),
+          from: path.resolve(__dirname, "src/public"),
+          to: path.resolve(__dirname, "dist"),
+          globOptions: {
+            ignore: ["**/images/heros**"],
+          },
         },
       ],
     }),
