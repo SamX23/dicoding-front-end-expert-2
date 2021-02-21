9cda8d77edbedbbedb01bbcc9cadb9a686e9a504
samx23 <skalexsong@gmail.com>
2021-02-21 11:38:58 +0700

    Remove comments

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,4 +1,4 @@
-import FavoriteDishIdb from "../../data/fetch-db";
+import FavoriteDish from "../../data/fetch-db";
 import FavoriteDishView from "./liked-dish/favorite-dish-view";
 import FavoriteDishShowPresenter from "./liked-dish/favorite-dish-show-presenter";
 
@@ -10,14 +10,7 @@ const Favorite = {
   },
 
   async afterRender() {
-    new FavoriteDishShowPresenter({ view, favoriteDish: FavoriteDishIdb });
-    // const favorite = await FavoriteDishIdb.getAllDish();
-    // const favouriteContainer = document.getElementById("card");
-    // if (favorite && favorite.length > 0) {
-    //   favorite.forEach((item) => {
-    //     favouriteContainer.innerHTML += createDishItemTemplate(item);
-    //   });
-    // }
+    new FavoriteDishShowPresenter({ view, favoriteDish: FavoriteDish });
   },
 };
 
