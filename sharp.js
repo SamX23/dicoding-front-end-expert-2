cb40195e9ad1f94bf793eea9a236aa3465b891f5
samx23 <skalexsong@gmail.com>
2021-04-29 13:25:28 +0700

    Update : change images folder name

--- sharp.js
+++ sharp.js
@@ -2,8 +2,8 @@ const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
 
-const target = path.resolve(__dirname, "src/public/images/heros");
-const destination = path.resolve(__dirname, "dist/images/heros");
+const target = path.resolve(__dirname, "src/public/images");
+const destination = path.resolve(__dirname, "dist/images");
 
 if (!fs.existsSync(destination)) {
   fs.mkdirSync(destination);
