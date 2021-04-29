050403293bdcba90f23520e1e934df362d2865db
samx23 <skalexsong@gmail.com>
2021-04-29 15:06:10 +0700

    Update : remove cleanup plugin and add next gen image plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,11 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ServiceWorkerWebpackPlugin = require("serviceworker-webpack-plugin");
 const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
 const ImageminMozjpeg = require("imagemin-mozjpeg");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
-const path = require("path");
+const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "src/scripts/index.js"),
@@ -53,7 +52,6 @@ module.exports = {
     },
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/templates/index.html"),
       filename: "index.html",
@@ -64,7 +62,7 @@ module.exports = {
           from: path.resolve(__dirname, "src/public"),
           to: path.resolve(__dirname, "dist"),
           globOptions: {
-            ignore: ["**/images/heros**"],
+            ignore: ["**/images/**"],
           },
         },
       ],
@@ -79,6 +77,17 @@ module.exports = {
           progressive: true,
         }),
       ],
+    }),
+    new ImageminWebpWebpackPlugin({
+      config: [
+        {
+          test: /\.(jpe?g|png)/,
+          options: {
+            quality: 50,
+          },
+        },
+      ],
+      overrideExtension: true,
     }),
     new BundleAnalyzerPlugin(),
   ],
