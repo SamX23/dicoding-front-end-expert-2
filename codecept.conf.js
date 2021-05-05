84a30a746fb61cf9ca0d4f2dd14f3a8929ade055
samx23 <skalexsong@gmail.com>
2021-05-05 19:19:53 +0700

    Update : formatted by prettier

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,35 +1,35 @@
-const { setHeadlessWhen } = require('@codeceptjs/configure');
+const { setHeadlessWhen } = require("@codeceptjs/configure");
 
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
 
 exports.config = {
-  tests: 'e2e/**/*.spec.js',
-  output: 'e2e/outputs',
+  tests: "e2e/**/*.spec.js",
+  output: "e2e/outputs",
   helpers: {
     Puppeteer: {
-      url: 'http://localhost:8080',
+      url: "http://localhost:8080",
       show: true,
-      windowSize: '1200x900'
-    }
+      windowSize: "1200x900",
+    },
   },
   include: {
-    I: './steps_file.js'
+    I: "./steps_file.js",
   },
   bootstrap: null,
   mocha: {},
-  name: 'dicoding-front-end-expert-2 (submission)',
+  name: "dicoding-front-end-expert-2 (submission)",
   plugins: {
     pauseOnFail: {},
     retryFailedStep: {
-      enabled: true
+      enabled: true,
     },
     tryTo: {
-      enabled: true
+      enabled: true,
     },
     screenshotOnFail: {
-      enabled: true
-    }
-  }
-}
+      enabled: true,
+    },
+  },
+};
