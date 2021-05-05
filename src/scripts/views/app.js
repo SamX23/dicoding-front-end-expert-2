38cd81431bf3e16dc698eb0fa941dd5c79b5adb6
samx23 <skalexsong@gmail.com>
2021-05-05 19:20:37 +0700

    Update : implement scroll buton

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,6 +1,7 @@
 import Drawer from "../utils/show-drawer";
 import UrlParser from "../routes/url-parser";
 import routes from "../routes/routes";
+import scrollTop from "./template/scrollTop";
 
 class App {
   constructor({ button, drawer, content }) {
@@ -23,6 +24,7 @@ class App {
     const page = routes[url];
     this._content.innerHTML = await page.render();
     await page.afterRender();
+    scrollTop();
   }
 }
 
